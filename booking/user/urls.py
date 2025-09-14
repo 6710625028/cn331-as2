24ac108649fc3399d4c5f44ac94d5a826d1be55c
6710625028 <6710625028@student.tu.ac.th>
2025-09-14 17:19:32 +0700

    urls.py

--- booking/user/urls.py
+++ booking/user/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.room_list, name='room_list'),
-    path('book/<int:room_id>/', views.book_room, name='book_room'),
+    path(’‘, views.home, name=’home‘),
+    path(’login/‘, auth_views.LoginView.as_view(template_name=’login.html‘), name=’login‘),
+    path(’logout/‘, auth_views.LogoutView.as_view(), name=’logout‘),
 ]
