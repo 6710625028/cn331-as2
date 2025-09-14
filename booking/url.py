dcbdf854014a13fed4bae86c8ff29ab763c96883
6710625028 <6710625028@student.tu.ac.th>
2025-09-14 17:53:17 +0700

    url.py

---
+++ booking/url.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.room_list, name='room_list'),
+    path('book/<int:room_id>/', views.book_room, name='book_room'),
+    path('my-bookings/', views.my_bookings, name='my_bookings'),
+]
