183608e1be6bf2b807e5ead71aca534cc11c4b35
6710625028 <6710625028@student.tu.ac.th>
2025-09-14 17:35:04 +0700

    setting.py

--- booking/user/setting.py
+++ booking/user/setting.py
@@ -1,3 +1,15 @@
+INSTALLED_APPS = [
+    # ...
+    ’django.contrib.staticfiles‘,
+    ’crispy_forms‘,
+    ’crispy_bootstrap5‘,
+    ’bookings‘,
+]
+CRISPY_ALLOWED_TEMPLATE_PACKS = ”bootstrap5“
+CRISPY_TEMPLATE_PACK = ”bootstrap5“
+
+STATIC_URL = ’/static/‘
+
 LOGIN_URL = ’login‘
 LOGIN_REDIRECT_URL = ’/‘
 LOGOUT_REDIRECT_URL = ’login‘
